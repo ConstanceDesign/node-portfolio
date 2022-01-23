3cbe40eb35e2f181d99b1173c9aa3ae0087ffe13
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-01-23 14:07:24 -0500

    Modify the HTML template to handle more data

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-<<<<<<< HEAD
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 
@@ -139,30 +138,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
-=======
-const generatePage = require("./src/page-template.js");
-
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile("./index.html", pageHTML, (err) => {
-  if (err) throw err;
-
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
->>>>>>> feature/questions
